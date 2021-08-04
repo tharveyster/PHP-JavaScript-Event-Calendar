f296bd0d8af98d4349328ffbaeced14f38f6b561
tharvey <tharveyster@gmail.com>
2021-08-04 14:21:05 -0400

    Updated dbConfig file with database name

--- includes/dbConfig.php
+++ includes/dbConfig.php
@@ -3,7 +3,7 @@ date_default_timezone_set("America/New_York");
 
 try {
 
-	$con = new PDO("mysql:dbname=[DATABASE NAME];host=localhost", "[USERNAME]", "[PASSWORD]");
+	$con = new PDO("mysql:dbname=calendar;host=localhost", "USERNAME", "PASSWORD");
 
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
