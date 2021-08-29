ce659f565b9cc5663a714c78cf20085da85faf0a
Todd Harvey <tharveyster@gmail.com>
2021-08-29 06:18:22 -0400

    Added comment

--- includes/dbConfig.php
+++ includes/dbConfig.php
@@ -3,7 +3,7 @@ date_default_timezone_set("America/New_York");
 
 try {
 
-	$con = new PDO("mysql:dbname=calendar;host=localhost", "USERNAME", "PASSWORD");
+	$con = new PDO("mysql:dbname=calendar;host=localhost", "USERNAME", "PASSWORD"); // Change username and password to your database username and password
 
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
