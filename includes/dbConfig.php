dda7b629c546b68e8e8da4e60cf90a88b68754fa
Todd Harvey <tharveyster@gmail.com>
2022-03-16 21:32:08 -0400

    Reconfigured dbConfig file for Heroku deployment

--- includes/dbConfig.php
+++ includes/dbConfig.php
@@ -1,9 +1,18 @@
 <?php
 date_default_timezone_set("America/New_York");
 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
 try {
 
-	$con = new PDO("mysql:dbname=calendar;host=localhost", "USERNAME", "PASSWORD"); // Change username and password to your database username and password
+	$con = new PDO("mysql:dbname=$cleardb_db;host=$cleardb_server", $cleardb_username, $cleardb_password); // Change username and password to your database username and password
 
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
